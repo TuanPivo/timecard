fa9eec9257447b7616195d078acbad00c8b398e6
DuongVanTuan <duong.van.tuan@uruca.jp>
2024-06-13 11:30:20 +0700

    add model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Attendance;
+
 
 class HomeController extends Controller
 {
@@ -8,6 +12,20 @@ class HomeController extends Controller
     public function index()
     {
         return view('pages.home');
+    }
+
+    public function attendance(Request $request){
+        $user = Auth::user();
+        $type = $request->input('type');
+        $attendance = [
+            'user_id' => $user->id,
+            'date' => now()->toDateString(),
+            'type' => $type,
+            'status' => 'success',
+        ];
+        Attendance::create($attendance);
+        return redirect()->route('home');
+
     }
 
 }
